2042172b497df5f71abd790b173f5e5167fccc5e
Veng <vengtear1399@gmail.com>
2023-07-05 23:49:31 +0700

    commit-testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as fs from 'fs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+// import * as fs from 'fs';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
@@ -20,16 +20,16 @@ import { UserModule } from './user/user.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
+      port: parseInt(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       entities: [`${__dirname}/**/*.entity{.js,.ts}`],
-      autoLoadEntities: true,
-      ssl: {
-        ca: fs.readFileSync(process.env.SSL_CA_CERTIFICATES),
-      },
-      synchronize: true,
+      // autoLoadEntities: true,
+      // ssl: {
+      //   ca: fs.readFileSync(process.env.SSL_CA_CERTIFICATES),
+      // },
+      // synchronize: true,
     }),
     AuthModule,
     UserModule,
